afba1c41a11e55845d111d5dfddac5b25d2c91e1
JimmyT64 <JimmyT64@hotmail.com>
2024-12-18 13:03:23 +0000

    Added asteroids, collision logic and a fail state

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+    def collision(self,other):
+        r = False
+        if self.position.distance_to(other.position) <= self.radius + other.radius:
+            r = True
+        return r
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updateable)
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids,drawable,updateable)
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     pygame.init
     Clock = pygame.time.Clock()
@@ -15,11 +24,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updateable.update(dt)
         screen.fill("black",rect = None, special_flags = 0)
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60)/1000
+        for ast in asteroids:
+            if ast.collision(player) == True:
+                print("Game over!")
+                exit()
+        
         
 
     
