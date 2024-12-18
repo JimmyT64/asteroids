3f04cf978a07c80ff3abddf34ec4f6e2010ad08e
JimmyT64 <JimmyT64@hotmail.com>
2024-12-18 11:38:46 +0000

    Added Triangle and now you can move it with WASD

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 from constants import *
+from player import *
+
 def main():
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     pygame.init
+    Clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -10,8 +15,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black",rect = None, special_flags = 0)
+        player.draw(screen)
         pygame.display.flip()
+        dt = Clock.tick(60)/1000
+        
 
     
 if __name__ == "__main__":
